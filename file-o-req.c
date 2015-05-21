eb7d5429b61a65a7d9d4103dd014d342ea44a737
Michael Christa <michael-christa@t-online.de>
2015-05-21 06:13:20 -0700

    notes

--- file-o-req.c
+++ file-o-req.c
@@ -1,3 +1,5 @@
+#define BUFSIZE 1000
+
 int main(int argc, char **argv){
     int port;
     port = atoi(argv[1]);
@@ -24,7 +26,40 @@ static int accept_clients(int sd){
     
     while(1){
         from_client_len = sizeof(from_client);
-        nsd = accept(sd,(struct sockaddr*)&from_client, &from_client_len) //generische Struktur wird erwartet, Länge der Socketaddresse
+        nsd = accept(sd/*in*/,(struct sockaddr*)&from_client/*in out*/, &from_client_len/*in out*/) //generische Struktur wird erwartet, Länge der Socketaddresse
                 
+        //Fehler bei nsd <0 -> Fehler: break
+                
+                
+        //Immer nur ein Client kann am Server arbeiten. Es empfiehlt sich über fork den neuen Prozess einzuhängen        
+        handle_client(nsd);
+        
     }
+    return nsd;
+}
+
+//Static bedeutet, dass es in anderen Modulen nicht sichtbar ist
+static int handle_client(int sd){
+    char buf[BUFSIZE];
+    int cc; //Character count
+    while( (cc = read(sd, buf, BUFSIZE)) > 0){
+        //read drei fälle
+        if(cc < 0){ // -> Das muss dann außerhalb der Schleife sein
+            printf("Error");
+        }else if(cc == 0){
+            //Kommunikationspartner hat die Verbindung beendet
+        }else{
+            
+        }
+        
+        if(write(sd,buf,cc)<0){
+            // Fehler
+        }
+        
+        
+        
+    }
+    close(sd);
+    return(sd);
+    
 }
